5af1fa15460882486bb53ac78ecf041144360573
PeshalPerera <peshala.20201256@iit.ac.lk>
2025-04-05 09:02:21 +0530

    feat: implement login screen with form and JWT token storage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Register from './pages/Register'
-// (later you'll add Login, Dashboard, etc.)
+import Login from './pages/Login'
+import Dashboard from './pages/Login'
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       {/* more routes coming soon */}
     </Routes>
   )
